cdf6418bac737e24767edb69e7ca1e359ee772da
Molicheu <64894726+Molicheu@users.noreply.github.com>
2024-08-07 11:46:42 -0400

    Refactored shutdown logic for better readability and organization

--- client.lua
+++ client.lua
@@ -1,17 +1,20 @@
+-- Variables
+local firstSpawn = false
 local disableManualShutdown = GetConvarInt('pe-basicloading:disableAutoShutdown', 0) == 1
 
+-- Function to handle the shutdown process
 local function shutdownHandler()
-    CreateThread(function()
-        SendLoadingScreenMessage(json.encode({
-            fullyLoaded = true
-        }))
-        Wait(5000)
+    if not firstSpawn then
+        firstSpawn = true
+        SendLoadingScreenMessage(json.encode({ fullyLoaded = true }))
         ShutdownLoadingScreenNui()
-    end)
+    end
 end
 
+-- Register event handler if manual shutdown is not disabled
 if not disableManualShutdown then
     AddEventHandler('playerSpawned', shutdownHandler)
 end
 
+-- Export the shutdown function
 exports('shutdown', shutdownHandler)
